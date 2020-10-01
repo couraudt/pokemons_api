7671e5a7163f4456be65c9af0afe98ffb1ebc089
Thibault Couraud <1036233+couraudt@users.noreply.github.com>
2020-10-01 14:22:09 +0200

    add test for pokemon model

---
+++ spec/models/pokemon_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Pokemon do
+  it 'validate uniqueness of name' do
+    described_class.create(name: 'Pikachu')
+    expect { described_class.create!(name: 'Pikachu') }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+end
